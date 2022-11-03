a3472618058976c4a472865688eebf0bac3c3f41
buenoleo67 <111444675+buenoleo67@users.noreply.github.com>
2022-11-02 21:09:26 -0300

    Update index.php

--- index.php
+++ index.php
@@ -11,31 +11,22 @@
     <img src="logo2.jpg" alt="logomarca empresa">
    <div class = "txt"><h1>Ztech Assistência Técnica</h1> <h3>A assistência que precisa esta aqui! <p></p> </h3>
    <h3> Atuamos na área de consertos em <b>Notebooks,</b> <b>Impressoras,</b> <b>Celulares</b> e <b>Televisores</b></h3>
-   <nav id="menu">
-   <ul>
-        <li> <a href="sobre.php" target = "blank">Quem somos</a></li>
-        <li> <a href="login.php" target = "blank">Ja é cliente? Faça login</a></li>
-        <li> <a href="cadastro.php" target = "blank">Primeiro acesso? Faça seu cadastro</a></li>
-        <li> <a href="login.php" target = "blank">Verificar status do Produto</a></li>
-        </nav>
-        <br><br>
-    </ul>
-    </div>
-    <div class= "info">
-    <h2><b> Av. Maringá 920, loja 05 </b></h2> 
-    <nav id="link">
-    <h4>Link para<a href="https://www.google.com/maps/place/ZTECH+ASSIST%C3%8ANCIA+T%C3%89CNICA/@-23.3097886,-51.1805102,17z/data=!3m1!4b1!4m5!3m4!1s0x94eb45e28a0cbf1d:0x81125ba5c6346b00!8m2!3d-23.3097935!4d-51.1783215" target = "blank"> maps</a></h4>
-    <h4>Link para <a href="https://l.instagram.com/?u=http%3A%2F%2Fwa.me%2F5543991327778%2F&e=ATOwDL1PHlSDAhamZhUbq1pXqF99Ra0z9ndEgocuhl_Nlpdgt0ct0E4wzqEp_yapCtc-loIWV4xqJDz9Rp9bTKSTgHcy1g3vm8QeEYM&s=1" target = "blank">Whatsapp</a></h4>  
-    <h4>Link para <a href="https://www.facebook.com/profile.php?id=100070395370197" target ="blank">Facebook</a> </h4>
-    </nav>
+   <img id="cel" src="celular.jpeg" alt="celular">
+   <img  id="comp" src="computador.jpeg" alt="computador">
+   <img id="molhou" src="molhou.jpeg" alt="celmolhado">
     </div>
     <div>
     <ul id="menux">
+        <nav id="menu">
         <li> <a href="sobre.php" target = "blank">Quem somos</a></li>
         <li> <a href="login.php" target = "blank">Faça login</a></li>
         <li> <a href="cadastro.php" target = "blank">Faça seu cadastro</a></li>        
         <br><br>
     </ul>
+    </nav>
+    </div>
+    <div>
+         
     </div>
 </body>
 </html>
