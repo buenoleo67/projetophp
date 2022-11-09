7f1451cf59e1891befa62cd3bfdf3a2b417bb546
buenoleo67 <111444675+buenoleo67@users.noreply.github.com>
2022-11-08 21:11:45 -0300

    Update registro.php

--- registro.php
+++ registro.php
@@ -7,18 +7,27 @@
     <link rel="stylesheet" href="registro.css">
     <title>registro</title>
 </head>
-<body bgcolor="black">
-   <h1> Bem vindo! o seu registro foi recebido com sucesso! </h1>
-   <div id="card">
+<body bgcolor="white">
+   <h1>   </h1>
     <?php
+    include 'ztechbanco.php';
     $nome = $_POST['nome'];
-    echo " <h2>$nome, é otimo ter você conosco!</h2> <br>";
-        foreach($_POST as $cliente  => $dados){
-            echo "
-            $cliente : $dados<br> ";
+    $email = $_POST['email'];
+    $telefone = $_POST['telefone'];
+    $cpf = $_POST['cpf'];
+    $aparelho = $_POST['aparelho'];
+    if($nome != "" and $cpf != ""){
+        // Escreve o comando
+        $sql = "INSERT INTO cliente (nome , email , telefone , cpf , aparelho) VALUES ('$nome', '$email', '$telefone', '$cpf', '$aparelho')";
+        // Executa o comando
+        $banco->query($sql);
+        if($banco->affected_rows > 0){
+            echo "Cliente $nome cadastrado com sucesso!";
+        }else{
+            echo "Erro ao cadastrar Cliente $nome";
         }
-    ?>
-    </div>
+    }
+?>
     <h3>Retornaremos o contato o mais breve possível!</h3>
     <b><h3>Ou acesse nosso whatsapp </b> <a href="https://l.instagram.com/?u=http%3A%2F%2Fwa.me%2F5543991327778%2F&e=ATOwDL1PHlSDAhamZhUbq1pXqF99Ra0z9ndEgocuhl_Nlpdgt0ct0E4wzqEp_yapCtc-loIWV4xqJDz9Rp9bTKSTgHcy1g3vm8QeEYM&s=1" target = "blank">Whatsapp</a></h3>  
     
